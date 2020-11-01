21c8ad93538157a2974b7c9d843ad05d47a7be1a
Christian Musa <christianmusa@gmail.com>
2020-11-01 15:15:05 -0300

    feat(api-bridge-gh): add api baseline to kickoff ci setup

---
+++ packages/api-bridge-gh/lambda.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function dummyHandler(event) {
+  console.info('Event', event);
+  console.info('Event', process.env);
+  return {
+    statusCode: 200,
+    body: 'hi from dummy'
+  };
+}
+
+module.exports = {
+  dummyHandler
+};
